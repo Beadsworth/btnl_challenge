871061871cf5822f2e3e9996ea724b9e4f686ed4
James Beadsworth <james.beadsworth@intelsat.com>
2023-12-02 14:21:08 -0600

    added day template

---
+++ solutions/template/Soln.hs
@@ -0,0 +1,14 @@
+-- day00
+module Solution.Soln  
+( solve1  
+, solve2  
+) where 
+
+
+-- solutions
+solve1 :: String -> Int
+solve1 contents = 0
+
+-- the words overlap!
+solve2 :: String -> Int
+solve2 contents = 0
