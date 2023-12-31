349d35bf32f6654a9f2ffae39551b1b0d5478015
James Beadsworth <james.beadsworth@intelsat.com>
2023-12-31 13:54:02 -0600

    solved part 1

--- solutions/day05/Soln.hs
+++ solutions/day05/Soln.hs
@@ -11,19 +11,24 @@ import qualified Data.Text as T
 import qualified Data.Map as Map
 
 
-specialStrings = 
-    [ "seeds:"
-    , "seed-to-soil map:"
-    , "soil-to-fertilizer map:"
-    , "fertilizer-to-water map:"
-    , "water-to-light map:"
-    , "light-to-temperature map:"
-    , "temperature-to-humidity map:"
-    , "humidity-to-location map:" ]
+type SectionMap = Map.Map String [(Int, Int, Int)]
 
 
+mapKeys = 
+    [ "seed-to-soil"
+    , "soil-to-fertilizer"
+    , "fertilizer-to-water"
+    , "water-to-light"
+    , "light-to-temperature"
+    , "temperature-to-humidity"
+    , "humidity-to-location" ]
 
-parseMap :: String -> ([Int], Map.Map String [[Int]])
+
+tuplify3 :: [a] -> (a,a,a)
+tuplify3 [x,y,z] = (x,y,z)
+
+
+parseMap :: String -> ([Int], SectionMap)
 parseMap contents = (seeds, sectionMap)
     where
         noLastLine = T.unpack (T.strip (T.pack contents))
@@ -32,20 +37,55 @@ parseMap contents = (seeds, sectionMap)
         nonSeedSections = drop 1 cleanSections
         sectionPreMap = [ (head (take 1 sec), (drop 1 sec)) | sec <- nonSeedSections ]
         sectionPreMap2 = [ (head (splitOn " map:" k), [ [ read c :: Int | c <- (splitOn " " line) ] | line <- v]) | (k, v) <- sectionPreMap]
-        sectionMap = Map.fromList sectionPreMap2
+        sectionPreMap3 = [ (k, [tuplify3 x | x <- v ] ) | (k, v) <- sectionPreMap2 ]
+        sectionMap = Map.fromList sectionPreMap3
         
         -- get seeds
         seedsLine = head sections
         seedsNums = last (splitOn "seeds: " seedsLine)
-        seeds = [ read seed | seed <- (splitOn " " seedsNums) ] :: [Int] 
+        seeds = [ read seed | seed <- (splitOn " " seedsNums) ] :: [Int]
+
+
+checkNumRange :: Int -> Int -> Int -> Bool
+checkNumRange sourceStart rangeLength sourceNum = withinRange
+    where withinRange = (sourceStart <= sourceNum) && (sourceNum < (sourceStart + rangeLength))
+
+
+mapLookup :: [(Int, Int, Int)] -> Int -> Int
+mapLookup mapList sourceNum
+    | (length mapList) == 0 = sourceNum
+    | checkNumRange sourceStart rangeLength sourceNum = dest
+    | otherwise = mapLookup subMapList sourceNum
+        where
+            subMapList = drop 1 mapList
+            (destStart, sourceStart, rangeLength) = head mapList 
+            dest = destStart + (sourceNum - sourceStart)
+
+
+traverseMap :: SectionMap -> [String] -> Int -> Int
+traverseMap sectionMap keyList input
+    | (length keyList) < 1 = input
+    | otherwise = traverseMap sectionMap subKeyList result
+        where
+            currentKey = head keyList
+            subKeyList = drop 1 keyList
+            mapList = sectionMap Map.! currentKey
+            result = mapLookup mapList input
+
 
 
 -- solution part 1
--- solve1 :: String -> a
-solve1 contents = seeds
+solve1 :: String -> Int
+solve1 contents = result
     where
         (seeds, sectionMap) = parseMap contents
         
+        getLocation :: (Int -> Int)
+        getLocation = traverseMap sectionMap mapKeys
+        
+        seedLocations = [ getLocation seed | seed <- seeds ]
+        result = minimum seedLocations
+
         
 
 -- solution part 2
