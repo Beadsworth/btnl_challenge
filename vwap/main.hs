dc2fdd0913c0783f429c36d6d5fac2f73ae90660
James Beadsworth <james.beadsworth@intelsat.com>
2024-10-06 16:22:44 -0500

    some testing

--- vwap/main.hs
+++ vwap/main.hs
@@ -7,9 +7,32 @@ main = do
 
     putStrLn "thinking..."
 
-    let match = Match { makerAcntID="maker", takerAcntID="taker", prodSym="prod sym", takerSide=Ask, price=1000, quantity=1}
+    let match1 = Match  { makerAcntID = "Tyrell Corp A123"
+                        , takerAcntID = "Wayland-Yutani Corp BC32"
+                        , prodSym = "BUSU1"
+                        , takerSide = Bid
+                        , price = 42
+                        , quantity = 10
+                        }
+    let match2 = Match  { makerAcntID = "CHOAM Arakis Z23"
+                        , takerAcntID = "OPEC 897"
+                        , prodSym = "BUIZ1"
+                        , takerSide = Ask
+                        , price = (-2)
+                        , quantity = 14
+                        }
+    
+    let match3 = Match  { makerAcntID = "InGen Tech BCZ232"
+                        , takerAcntID = "BioSynFG332"
+                        , prodSym = "BUSM2"
+                        , takerSide = Bid
+                        , price = 43250
+                        , quantity = 23
+                        }
 
-    putStrLn (show match)
+    putStrLn (show match1)
+    putStrLn (show match2)
+    putStrLn (show match3)
     -- contents <- getContents
     -- putStrLn contents
 
