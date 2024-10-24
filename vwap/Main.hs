54fdbd1b7633c5ea3ff30d83921daf9ba0698ca6
James Beadsworth <james.beadsworth@intelsat.com>
2024-10-24 02:50:19 -0500

    put csv parsing in loop

--- vwap/Main.hs
+++ vwap/Main.hs
@@ -6,9 +6,12 @@ import qualified Data.Csv.Streaming as CsvS
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy.Char8 as BSL8
 import qualified Data.Foldable as F
-import System.IO (stdin)
+import System.IO (stdin, hIsEOF)
 import Control.Concurrent (newMVar, modifyMVar_, readMVar)
 import Control.Monad.IO.Class (liftIO)
+import Control.Exception (SomeException, AsyncException(UserInterrupt), catch, throwIO)
+import Control.Monad (forever)
+
 
 
 import VWAP.In (Match, updateSumsMap, emptySumsMap)
@@ -20,19 +23,28 @@ main = do
     -- start MVar with empty sumsMap
     sumsMapVar <- newMVar emptySumsMap
 
-    -- Read CSV data from stdin as lazy ByteString
-    input <- BSL8.hGetContents stdin
-    
-    -- parse the CSV using streaming
-    -- if incoming CSV is large, this 
-    -- will hopefully prevent large memory consumption
-    let csvStream = CsvS.decode Csv.NoHeader input :: CsvS.Records Match
+    let readStdin = forever $ do
+            eof <- hIsEOF stdin
+            if eof
+                then throwIO UserInterrupt  -- This will break out of the forever loop
+                else do
+                    line <- BS8.hGetLine stdin
 
-    -- fold over the stream to accumulate totals
-    -- strict foldl prevents too much thunking
-    modifyMVar_ sumsMapVar $ \sumsMap ->
-        return $ F.foldl' updateSumsMap sumsMap csvStream
+                    -- convert to lazy ByteString
+                    let input = BSL8.fromStrict line
+                    
+                    -- parse the CSV using streaming
+                    -- if incoming CSV is large, this 
+                    -- will hopefully prevent large memory consumption
+                    let csvStream = CsvS.decode Csv.NoHeader input :: CsvS.Records Match
 
+                    -- fold over the stream to accumulate totals
+                    -- strict foldl prevents too much thunking
+                    modifyMVar_ sumsMapVar $ \sumsMap ->
+                        return $ F.foldl' updateSumsMap sumsMap csvStream
+
+    readStdin `catch` handleInterrupt
+  
     -- Grab the latest sumsMap
     sumsMap <- liftIO $ readMVar sumsMapVar
 
@@ -41,3 +53,7 @@ main = do
     
     -- print Report to stdout
     BSL8.putStrLn reportJSON
+
+  where
+    handleInterrupt :: SomeException -> IO ()
+    handleInterrupt _ = return ()
