23be0e60a8d38483ffa7183aa995786e22df5661
James Beadsworth <james.beadsworth@intelsat.com>
2024-10-24 03:33:09 -0500

    decompose readFromStdin

--- vwap/Main.hs
+++ vwap/Main.hs
@@ -7,15 +7,38 @@ import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy.Char8 as BSL8
 import qualified Data.Foldable as F
 import System.IO (stdin, hIsEOF)
-import Control.Concurrent (newMVar, modifyMVar_, readMVar, forkIO)
+import Control.Concurrent (MVar, newMVar, modifyMVar_, readMVar, forkIO)
 import Control.Monad.IO.Class (liftIO)
 import Control.Exception (SomeException, AsyncException(UserInterrupt), catch, throwIO)
 import Control.Monad (forever)
 import qualified Web.Scotty as Scotty
 
 
-import VWAP.In (Match, updateSumsMap, emptySumsMap)
+import VWAP.In (Match, SumsMap, updateSumsMap, emptySumsMap)
 import VWAP.Out (sumsMap2ReportJSON)
+
+
+-- Function to read from stdin and update the sumsMap
+readFromStdin :: MVar SumsMap -> IO ()
+readFromStdin sumsMapVar = forever $ do
+    eof <- hIsEOF stdin
+    if eof
+        then throwIO UserInterrupt  -- This will break out of the forever loop
+        else do
+            line <- BS8.hGetLine stdin
+
+            -- convert to lazy ByteString
+            let input = BSL8.fromStrict line
+            
+            -- parse the CSV using streaming
+            -- if incoming CSV is large, this 
+            -- will hopefully prevent large memory consumption
+            let csvStream = CsvS.decode Csv.NoHeader input :: CsvS.Records Match
+
+            -- fold over the stream to accumulate totals
+            -- strict foldl prevents too much thunking
+            modifyMVar_ sumsMapVar $ \sumsMap ->
+                return $ F.foldl' updateSumsMap sumsMap csvStream
 
 
 main :: IO ()
@@ -24,30 +47,8 @@ main = do
     -- start MVar with empty sumsMap
     sumsMapVar <- newMVar emptySumsMap
 
-    -- thread to read from stdin
-    let readStdin = forever $ do
-            eof <- hIsEOF stdin
-            if eof
-                then throwIO UserInterrupt  -- This will break out of the forever loop
-                else do
-                    line <- BS8.hGetLine stdin
-
-                    -- convert to lazy ByteString
-                    let input = BSL8.fromStrict line
-                    
-                    -- parse the CSV using streaming
-                    -- if incoming CSV is large, this 
-                    -- will hopefully prevent large memory consumption
-                    let csvStream = CsvS.decode Csv.NoHeader input :: CsvS.Records Match
-
-                    -- fold over the stream to accumulate totals
-                    -- strict foldl prevents too much thunking
-                    modifyMVar_ sumsMapVar $ \sumsMap ->
-                        return $ F.foldl' updateSumsMap sumsMap csvStream
-
     -- spawn the stdin reader in a separate thread using forkIO
-    _ <- forkIO $ readStdin `catch` handleInterrupt
-
+    _ <- forkIO $ (readFromStdin sumsMapVar) `catch` handleInterrupt
 
     -- start the scotty server
     Scotty.scotty 3000 $ do
@@ -65,6 +66,6 @@ main = do
             -- set response
             Scotty.raw reportJSON
 
-  where
-    handleInterrupt :: SomeException -> IO ()
-    handleInterrupt _ = return ()
+    where
+        handleInterrupt :: SomeException -> IO ()
+        handleInterrupt _ = return ()
